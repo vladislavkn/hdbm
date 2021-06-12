d050448c4adf95fd61f4ea69a170504c4844bf44
Vladislav Knyshov <51641565+vladislavkn@users.noreply.github.com>
2021-06-12 10:35:06 +0300

    fix _app dispatch function import

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,19 +8,17 @@ import store from "@root/lib/store";
 import { Provider } from "react-redux";
 import "@root/styles/globals.css";
 import MessagesBundle from "@components/MessagesBundle";
-import { useDispatch } from "@root/lib/hooks/typedStoreHooks";
 import { tryToLoginWithSavedToken } from "@root/lib/slices/auth";
 
 export default function MyApp(props: AppProps) {
   const { Component, pageProps } = props;
-  const dispatch = useDispatch();
 
   useEffect(() => {
     // Clear server stylesheets
     const jssStyles = document.querySelector("#jss-server-side");
     if (jssStyles) jssStyles.parentElement.removeChild(jssStyles);
     // Auto login
-    dispatch(tryToLoginWithSavedToken());
+    store.dispatch(tryToLoginWithSavedToken());
   }, []);
 
   return (
