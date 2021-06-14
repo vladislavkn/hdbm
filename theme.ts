bbe19b87b6ec66f7a771b1427298dce9bcc84f90
Vladislav Knyshov <51641565+vladislavkn@users.noreply.github.com>
2021-06-14 16:02:48 +0300

    update typography fonts stack

--- theme.ts
+++ theme.ts
@@ -21,4 +21,18 @@ export default createMuiTheme({
     primary,
     secondary,
   },
+  typography: {
+    fontFamily: [
+      '-apple-system',
+      'BlinkMacSystemFont',
+      '"Segoe UI"',
+      'Roboto',
+      '"Helvetica Neue"',
+      'Arial',
+      'sans-serif',
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"',
+    ].join(','),
+  },
 });
