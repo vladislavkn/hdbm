8953e3b2435cc767fa3ebd660f114f6e4f674a47
Vladislav Knyshov <51641565+vladislavkn@users.noreply.github.com>
2021-06-15 17:34:38 +0300

    fix search page layout

--- pages/search.tsx
+++ pages/search.tsx
@@ -1,24 +1,11 @@
 import { useRouter } from "next/router";
 import { useEffect } from "react";
-import { makeStyles, Theme, Typography } from "@material-ui/core";
+import { makeStyles, Theme } from "@material-ui/core";
 import Layout from "@components/Layout";
 import SearchRoomsBundle from "@components/SearchRoomsBundle";
-
-const useStyles = makeStyles((theme: Theme) => ({
-  title: {
-    paddingBottom: theme.spacing(3),
-    fontWeight: "bold",
-    [theme.breakpoints.down("sm")]: {
-      fontSize: "2rem",
-    },
-    [theme.breakpoints.up("md")]: {
-      fontSize: "6rem",
-    },
-  },
-}));
+import Section from "@components/Section";
 
 const Search = () => {
-  const classes = useStyles();
   const { query } = useRouter();
   useEffect(() => {
     console.log(query);
@@ -26,8 +13,9 @@ const Search = () => {
 
   return (
     <Layout title="HDBM: поиск отелей">
-      <Typography className={classes.title}>Поиск отелей</Typography>
-      <SearchRoomsBundle />
+      <Section title="Поиск отелей">
+        <SearchRoomsBundle />
+      </Section>
     </Layout>
   );
 };
